17a08bec0c2ee4df96489007448a06160e4905fb
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-02-09 16:06:03 +0530

    hfhf.c

---
+++ hfhf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,c;
+	float b,d;
+	
+	printf("Enter MInutes");
+	scanf("%f",&b);
+	d=b/60;
+	printf("GIVEN MINUTES IN HOUR is %f",d);
+	printf("In Minutes is %f",b);
+	return 0;
+}
